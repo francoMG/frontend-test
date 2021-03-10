02008f1158a9aec01b70d8c7820677aca742e595
francoMG <francomolocheg99@gmail.com>
2021-03-09 22:53:09 -0500

    header

---
+++ src/components/App/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="flex items-center rounded-app bg-app-blue text-white py-3">
+    <h1 class="pl-8 font-bold text-2xl">
+      LOGO
+    </h1>
+
+    <div class="flex pl-24 gap-16">
+      <a
+        class="text-base"
+        v-for="(route, idx) in routes"
+        :href="route.to"
+        v-bind:key="idx"
+        >{{ route.title }}</a
+      >
+    </div>
+
+    <button class="ml-auto mr-6">
+      <img
+        class="rounded-full border-2 border-white w-10 h-10"
+        src="@/assets/profile.png"
+        alt="profile"
+      />
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      routes: [
+        {
+          to: '/#',
+          title: 'Menu',
+        },
+        {
+          to: '/#',
+          title: 'Menu',
+        },
+        {
+          to: '/#',
+          title: 'Menu',
+        },
+      ],
+    }
+  },
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 // tailwind.config.js
 module.exports = {
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false,
-    theme: {
-        extend: {},
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        'app-blue': '#1E1ADD',
+        'app-background': '#F7FAFC',
+      },
+      borderRadius: {
+        app: '5px',
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
