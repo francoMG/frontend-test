6d948a22ad4e4da331ca52047870ea5ac5bff8b0
francoMG <francomolocheg99@gmail.com>
2021-03-14 11:15:50 -0500

    test(user-table: inactive status

--- tests/unit/UsersTable.spec.js
+++ tests/unit/UsersTable.spec.js
@@ -1,22 +1,22 @@
 import { shallowMount } from '@vue/test-utils'
 import UsersTable from '@/components/shared/Users/UsersTable.vue'
 
+const users = [
+  {
+    department: 'Baby',
+    email: 'Yasmin_Murazik@yahoo.com',
+    first_name: 'Sidney',
+    id: '1',
+    lastSession: 1609293527111,
+    last_name: 'Halvorson',
+    profile_img: 'bad image url',
+    sessions: ['2020-12-30T01:58:47.111Z', '2020-04-10T16:24:38.281Z'],
+    status: true,
+  },
+]
+
 describe('Users table component', () => {
   it('load bad image url', () => {
-    const users = [
-      {
-        department: 'Baby',
-        email: 'Yasmin_Murazik@yahoo.com',
-        first_name: 'Sidney',
-        id: '1',
-        lastSession: 1609293527111,
-        last_name: 'Halvorson',
-        profile_img: 'bad image url',
-        sessions: ['2020-12-30T01:58:47.111Z', '2020-04-10T16:24:38.281Z'],
-        status: true,
-      },
-    ]
-
     const wrapper = shallowMount(UsersTable, {
       propsData: { users },
     })
@@ -24,5 +24,13 @@ describe('Users table component', () => {
     const img = wrapper.find('img')
 
     expect(img.src).not.toBe(users[0].profile_img)
+  })
+
+  it('inactive status', () => {
+    const wrapper = shallowMount(UsersTable, {
+      propsData: { users: [{ ...users[0], status: false }] },
+    })
+
+    expect(wrapper.text()).toContain('Inactive')
   })
 })
