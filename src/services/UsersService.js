8131789c9bcf674a862043d7b91fd4fca72b62e9
Matias Lopez <matiaslopezd@gmail.com>
2021-03-22 17:47:53 -0300

    NEW:Suggestions

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -2,9 +2,9 @@ export const getUsers = async () => {
   const userResponse = await fetch('/api/users')
   const { users } = await userResponse.json()
 
-  users.forEach(({ sessions }, idx) => {
+  users.forEach(({ sessions }, index) => {
     sessions = sessions.map((session) => new Date(session))
-    users[idx].lastSession = Math.max(...sessions)
+    users[index].lastSession = Math.max(...sessions)
   })
 
   return users
